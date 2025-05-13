1beeea072a331b2f1a79d51393e1cdeaf0cf438b
rajibulislam60 <rajibulislam60707877@gmail.com>
2025-05-13 22:20:39 +0600

    footer copyright and payment done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,44 +1,66 @@
 import React from "react";
 import Container from "./Container";
+import footerLogoImage from "../images/logo3.png";
+import paymentImage from "../images/img_1.png";
+import { FaRegCopyright, FaFacebookF, FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <div className="py-15 bg-gray-200">
       <Container>
-        <div className="flex justify-between">
-          <div>
-            <h4 className="text-2xl font-semibold mb-4">Company</h4>
-            <p className="text-gray-500">
-              Find a location nearest you. <br /> See Our Stores
-            </p>
-            <h5 className="text-gray-500">+880 1981-869112</h5>
-            <h6 className="text-gray-500">rajelissebeauty@gmail.com</h6>
+        <div>
+          <div className="flex justify-between">
+            <div>
+              <h4 className="text-2xl font-semibold mb-4">Company</h4>
+              <p className="text-gray-500">
+                Find a location nearest you. <br /> See Our Stores
+              </p>
+              <h5 className="text-gray-500">+880 1981-869112</h5>
+              <h6 className="text-gray-500">rajelissebeauty@gmail.com</h6>
+            </div>
+            <div>
+              <h4 className="text-2xl font-semibold mb-4">Useful Links</h4>
+              <ul className="text-gray-500">
+                <li>New Products</li>
+                <li>Best Sellers</li>
+                <li>Bundle & Save</li>
+                <li>Online Gift Card</li>
+              </ul>
+            </div>
+            <div>
+              <h4 className="text-2xl font-semibold mb-4">Information</h4>
+              <ul className="text-gray-500">
+                <li>Start a Return</li>
+                <li>Contact Us</li>
+                <li>Shipping FAQ</li>
+                <li>Terms & Conditions</li>
+                <li>Privacy Policy</li>
+              </ul>
+            </div>
+            <div>
+              <h4 className="text-2xl font-semibold mb-4">Good emails.</h4>
+              <p className="text-gray-500">
+                Enter your email below to be the first to know about new
+                collections and product <br /> launches.
+              </p>
+            </div>
           </div>
-          <div>
-            <h4 className="text-2xl font-semibold mb-4">Useful Links</h4>
-            <ul className="text-gray-500">
-              <li>New Products</li>
-              <li>Best Sellers</li>
-              <li>Bundle & Save</li>
-              <li>Online Gift Card</li>
-            </ul>
-          </div>
-          <div>
-            <h4 className="text-2xl font-semibold mb-4">Information</h4>
-            <ul className="text-gray-500">
-              <li>Start a Return</li>
-              <li>Contact Us</li>
-              <li>Shipping FAQ</li>
-              <li>Terms & Conditions</li>
-              <li>Privacy Policy</li>
-            </ul>
-          </div>
-          <div>
-            <h4 className="text-2xl font-semibold mb-4">Good emails.</h4>
-            <p className="text-gray-500">
-              Enter your email below to be the first to know about new
-              collections and product <br /> launches.
-            </p>
+          <div className="w-full h-[1px] bg-gray-600 my-5"></div>
+          <div className="flex justify-between items-center">
+            <div className="flex items-center gap-3">
+              <div className="flex gap-1 items-center text-xl text-gray-500">
+                <FaRegCopyright />
+                <h4>Rajelisse Beauty 2025</h4>
+              </div>
+              <FaFacebookF />
+              <FaInstagram />
+            </div>
+            <img
+              className="w-[100px]"
+              src={footerLogoImage}
+              alt="Footer Logo Image"
+            />
+            <img src={paymentImage} alt="Payment Image" />
           </div>
         </div>
       </Container>
