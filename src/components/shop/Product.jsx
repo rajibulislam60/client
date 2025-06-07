4f9ee91395006b8196fbaab12907dd91a070ef2e
rajibulislam60 <rajibulislam60707877@gmail.com>
2025-06-07 12:40:59 +0600

    add button and navigate the single product

--- src/components/shop/Product.jsx
+++ src/components/shop/Product.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router";
 // import productImage from "../../images/product1.jpg";
 
 const ProductItem = ({ data }) => {
@@ -28,13 +29,18 @@ const ProductItem = ({ data }) => {
   //     itemName: "shfksdfnksdnvk",
   //   },
   // ];
+  const navigate = useNavigate();
+  const handleClicktoId = () => {
+    navigate("/singleproduct");
+  };
 
   return (
     <div className="grid grid-cols-3 gap-3">
       {data.map((item) => (
         <div
           key={item.id}
-          className="w-[100%] md:w-[90%] h-[420px]  bg-white py-2 text-center shadow-md hover:shadow-lg transition-shadow duration-300"
+          className="w-[100%] md:w-[90%]  bg-white py-2 text-center shadow-md hover:shadow-lg transition-shadow duration-300"
+          onClick={handleClicktoId}
         >
           <div className="overflow-hidden cursor-pointer">
             <img
@@ -46,6 +52,11 @@ const ProductItem = ({ data }) => {
           <div className="text-gray-500 mt-3">
             <h4 className="text-xl font-semibold">Price: {item.price} TK</h4>
             <h3 className="text-lg font-semibold">{item.itemName}</h3>
+            <div className="py-3 px-5">
+              <button className="text-2xl font-semibold bg-teal-600 text-white hover:bg-teal-800 cursor-pointer py-2 w-full text-center rounded-md">
+                Add to Cart
+              </button>
+            </div>
           </div>
         </div>
       ))}
